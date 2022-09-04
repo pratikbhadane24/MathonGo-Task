bf68ad8b7d6e2703ca99a56e90e5c744e54047f7
pratikbhadane24 <pratikbhadane24@gmail.com>
2022-09-04 19:04:49 +0530

    completed register function

--- src/controller/auth.controller.ts
+++ src/controller/auth.controller.ts
@@ -4,12 +4,19 @@ import { User } from "../entity/user.entity";
 
 export const Register = async (req: Request, res: Response) => {
   const body = req.body;
-
   const user = getRepository(User).save({
     first_name: body.first_name,
     last_name: body.last_name,
     email: body.email,
     password: body.password,
   });
+
+  if (user) {
+    res.status(200).json({
+      message: "User Created Successfully",
+      data: await user,
+    });
+    return;
+  }
   res.send(user);
 };
