0c8cb1cf0b97fab43de428cf55a4e8522d0aa06a
pratikbhadane24 <pratikbhadane24@gmail.com>
2022-09-04 13:38:19 +0530

    initialized express app

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,13 @@
-console.log("Hello World!");
+import express, { Request, Response } from "express";
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World");
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}/`);
+});
