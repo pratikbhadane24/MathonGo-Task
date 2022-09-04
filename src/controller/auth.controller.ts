4a787687b55b6702db9d57ec6517aa1a2fe5e21a
pratikbhadane24 <pratikbhadane24@gmail.com>
2022-09-04 21:39:50 +0530

    password reset feature added

--- src/controller/auth.controller.ts
+++ src/controller/auth.controller.ts
@@ -40,3 +40,28 @@ export const Login = async (req: Request, res: Response) => {
     data: user,
   });
 };
+
+export const ResetPassword = async (req: Request, res: Response) => {
+  const user = await getRepository(User).findOne({ where: { email: req.body.email } });
+  const { new_password, confirm_new_password } = req.body;
+  if (!user) {
+    return res.status(404).send({
+      message: "User Not Found",
+    });
+  }
+  const old_password = user.password;
+  if (new_password !== confirm_new_password) {
+    return res.status(400).json({
+      message: "Password does not match",
+    });
+  }
+
+  await getRepository(User).update(user.id, {
+    password: new_password,
+  });
+
+  return res.status(200).json({
+    message: "Password Reset Successful",
+    data: { old_password: old_password, new_password: new_password },
+  });
+};
--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
-import { Login, Register } from "./controller/auth.controller";
+import { Login, Register, ResetPassword } from "./controller/auth.controller";
 
 export const routes = (router: Router) => {
   router.post("/api/register/", Register);
   router.post("/api/login/", Login);
+  router.put("/api/resetpassword/", ResetPassword);
 };
