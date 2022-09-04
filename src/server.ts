04175fecf559bc4697a5ab9ba4a5f3640141870b
pratikbhadane24 <pratikbhadane24@gmail.com>
2022-09-04 14:07:23 +0530

    connected to mongoDB Atlas

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,18 @@
 import express, { Request, Response } from "express";
+import { createConnection } from "typeorm";
+import dotenv from "dotenv";
+dotenv.config();
+
+createConnection({
+  type: "mongodb",
+  url: process.env.MONGODB_URL,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  logging: true,
+  synchronize: true,
+}).then(() => {
+  console.log("Connected to MongoDB🔥🔥");
+});
 
 const app = express();
 app.use(express.json());
@@ -7,7 +21,7 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Hello World");
 });
 
-const port = 3000;
+const port = process.env.PORT;
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}/`);
+  console.log(`Server is running on http://localhost:${process.env.PORT}/`);
 });
